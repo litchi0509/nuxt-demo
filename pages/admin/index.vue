aef61f52eb09c3de34eaab2ba1796f94f6f9fee0
litchi0509 <litchi0509@gmail.com>
2021-12-17 10:46:01 +0800

    init

---
+++ pages/admin/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Hello World!
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  transition: "page"
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <h1>Home page</h1>
+    <NuxtLink to="/admin">
+      admin
+    </NuxtLink>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
